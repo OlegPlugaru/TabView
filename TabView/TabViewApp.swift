a857dffa4cb93dcf6aa25092a4f9ee6262132b6f
Oleg Plugaru <oleg.plugaru.md@gmail.com>
2023-05-21 16:44:46 +0300

    Initial Commit

---
+++ TabView/TabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewApp.swift
+//  TabView
+//
+//  Created by Whoami on 21.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct TabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
